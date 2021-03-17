2f2a1e51c7d9e1c66f230c2c54d2eaf29e923c5c
Michael Cordell <mike@mikecordell.com>
2021-03-17 10:35:06 -0700

    Add nord theme to safe themes

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" default))
  '(safe-local-variable-values
    '((org-hugo-base-dir . "../hugo_test")
      (org-hugo-base-dir . "../test"))))
