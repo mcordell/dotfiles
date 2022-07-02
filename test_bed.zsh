ec3a1ba7ab2857a563cacee1ef142ac909b3a3ff
Michael Cordell <mike@mikecordell.com>
2022-07-02 11:54:12 -0700

    Modify test bed

--- test_bed.zsh
+++ test_bed.zsh
@@ -1,7 +1,11 @@
 #! /usr/bin/env zsh
 
+tested=$(find ./zsh/boot_strap_functions -type f | grep -v helpers | fzf)
 source zsh/boot_strap_functions/helpers
+source $tested
 
-source zsh/boot_strap_functions/$1
-
-$1
+if [[ -z $1 ]]; then
+	$(echo $tested | sed -e "s{./zsh/boot_strap_functions/{{")
+else
+	$1
+fi
