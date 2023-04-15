5205ae44fb0288a01b59883e060ffb23ac9fa438
Michael Cordell <mike@mikecordell.com>
2023-04-14 19:33:54 -0700

    nvim: add astronvim config

---
+++ astronvim_user_config/mappings.lua
@@ -0,0 +1,34 @@
+-- Mapping data with "desc" stored directly by vim.keymap.set().
+--
+-- Please use this mappings table to set keyboard mapping since this is the
+-- lower level configuration and more robust one. (which-key will
+-- automatically pick-up stored data by this setting.)
+return {
+  -- first key is the mode
+  n = {
+    -- second key is the lefthand side of the map
+    -- mappings seen under group name "Buffer"
+    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bD"] = {
+      function()
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
+      end,
+      desc = "Pick to close",
+    },
+    -- tables with the `name` key will be registered with which-key if it's installed
+    -- this is useful for naming menus
+    ["<leader>b"] = { name = "Buffers" },
+    -- quick save
+    ["<leader>fs"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    [",ta"] = { ":TestVisit<cr>", desc = "test visit" }, -- change description but the same command
+    [",tt"] = { ":TestFile<cr>", desc = "test file" },   -- change description but the same command
+    [",,"] = { ":b#<cr>", desc = "last buffer" },        -- change description but the same command
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    -- ["<esc>"] = false,
+  },
+}
---
+++ astronvim_user_config/plugins/user.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "shaunsingh/nord.nvim",
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
+  {
+    "tpope/vim-fugitive",
+    event = "User AstroGitFile"
+  },
+  {
+    "klen/nvim-test",
+    event = "VeryLazy",
+    config = function()
+      require('nvim-test').setup({
+        term = "toggleterm"
+      })
+      require('nvim-test.runners.rspec'):setup {
+        command = "bundle exec rspec", -- a command to run the test runner
+      }
+    end
+  },
+  {
+    "github/copilot.vim",
+    event = "VeryLazy",
+  }
+
+
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}
