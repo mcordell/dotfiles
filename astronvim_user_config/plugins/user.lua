948a4dc7779a4bf840ecfb2362ae3cd6e54311a2
Michael Cordell <mike@mikecordell.com>
2023-04-30 09:21:15 -0700

    Add astronvim user config

--- astronvim_user_config/plugins/user.lua
+++ astronvim_user_config/plugins/user.lua
@@ -17,7 +17,7 @@ return {
 
   {
     "tpope/vim-fugitive",
-    event = "User AstroGitFile"
+    lazy = false
   },
   {
     "klen/nvim-test",
@@ -38,9 +38,31 @@ return {
   {
     "IndianBoy42/tree-sitter-just",
     event = "VeryLazy",
+    config = function()
+      require("nvim-treesitter.parsers").get_parser_configs().just = {
+        install_info = {
+          url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+          files = { "src/parser.c", "src/scanner.cc" },
+          branch = "main",
+          use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
+        },
+        maintainers = { "@IndianBoy42" },
+      }
+    end
   },
   {
     "mg979/vim-visual-multi",
+    event = "VeryLazy"
+  },
+  {
+    "dpayne/CodeGPT.nvim",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+    },
+    config = function()
+      require("codegpt.config")
+    end,
     event = "VeryLazy"
   }
   -- You can also add new plugins here as well:
