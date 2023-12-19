7a345c1c318c1f4c9444a193bde65651772c692c
Michael Cordell <mike@mikecordell.com>
2023-12-18 16:34:23 -0800

    nvim: add elixir-tools

--- astronvim_user_config/plugins/user.lua
+++ astronvim_user_config/plugins/user.lua
@@ -14,7 +14,6 @@ return {
       })
     end
   },
-
   {
     "tpope/vim-fugitive",
     lazy = false
@@ -41,6 +40,35 @@ return {
         }
       })
     end
+  },
+  {
+    "elixir-tools/elixir-tools.nvim",
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local elixir = require("elixir")
+      local elixirls = require("elixir.elixirls")
+
+      elixir.setup {
+        nextls = { enable = true },
+        credo = {},
+        elixirls = {
+          enable = true,
+          settings = elixirls.settings {
+            dialyzerEnabled = false,
+            enableTestLenses = true,
+          },
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          end,
+        }
+      }
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
   },
   {
     "IndianBoy42/tree-sitter-just",
