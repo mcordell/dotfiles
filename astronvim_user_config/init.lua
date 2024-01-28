7ad5a7186cc30d3aef987874e907b690546fece6
Michael Cordell <mike@mikecordell.com>
2024-01-27 19:35:48 -0800

    nvim: Fixes for ruby files

--- astronvim_user_config/init.lua
+++ astronvim_user_config/init.lua
@@ -40,21 +40,21 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 6000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
     },
     -- enable servers that you already have installed without mason
-    servers = {
-      "rubocop",
-    },
-    config = {
-      rubocop = {
-        command = "bundle",
-        args = { "exec", "rubocop", "--lsp" }
-      }
-    }
+    -- servers = {
+    --   "rubocop",
+    -- },
+    -- config = {
+    --   rubocop = {
+    --     command = "bundle",
+    --     args = { "exec", "rubocop", "--lsp" }
+    --   }
+    -- }
 
   },
   -- Configure require("lazy").setup() options
