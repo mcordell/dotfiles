aaa8adb6c379a0f024e0f45db22e966f0a73d9e1
Michael Cordell <mike@mikecordell.com>
2025-09-16 11:29:54 -0700

    doom: refactor out mappings specific config file

--- config/doom/config.el
+++ config/doom/config.el
@@ -380,51 +380,5 @@
   :commands (qcentrix-add-meetings)
   :after org           ; if you want to enforce load order
   )
-;; Keymaps
 
-(map! :leader
-      (:prefix "TAB"
-               "TAB" #'evil-switch-to-windows-last-buffer)
-      (:prefix "f"
-               ("a" (lambda ()
-                      (interactive)
-                      (org-agenda nil "w")))
-               ("t"  #'treemacs)
-               ("w"  #'+default/search-project)
-               )
-      (:prefix "s"
-               ("c"  #'evil-ex-nohighlight))
-      (:prefix "m"
-               (:map org-mode-map
-                :nv "j" #'mcordell/insert-rrp-jira
-                )
-               )
-      (:prefix "o"
-               ("c" #'cfw:open-org-calendar))
-      )
-
-(map! :prefix ","
-      (:map emacs-lisp-mode-map
-       :nv "f"
-       #'elisp-format-buffer)
-      :nv "t" #'projectile-toggle-between-implementation-and-test
-      (:map org-mode-map
-       :nv "o" #'org-open-at-point
-       :nv "b" #'helm-bibtex
-       :nv "e" #'org-mac-mailmate-insert-selected
-       :nv "f" #'org-mac-finder-item-get-selected
-       :nv "l" #'org-mac-link-brave-insert-frontmost-url
-       :nv "r" #'helm-org-rifle)
-      (:nv "," #'evil-switch-to-windows-last-buffer)
-      (:prefix "d"
-               (:map org-mode-map
-                :nv "t" #'org-time-stamp))
-      (:prefix ("s" . "subtree")
-               (:map org-mode-map
-                :nv "h" #'org-promote-subtree
-                :nv "l" #'org-demote-subtree
-                :nv "r" #'org-refile
-                :nv "a" #'org-archive-subtree))
-      (:prefix ("m" . "mix")
-               (:map elixir-mode-map
-                :nv "m" #'alchemist-mix)))
+(load! "+bindings")
