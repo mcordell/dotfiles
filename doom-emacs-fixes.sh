9b3662951c10c193e89174995c88d69dd8c8bdd5
Michael Cordell <mike@mikecordell.com>
2025-10-28 11:05:01 -0700

    doom: add script for installing common doom deps
    Mainly stuff that doom doctor complains about

---
+++ doom-emacs-fixes.sh
@@ -0,0 +1,7 @@
+brew install shfmt
+go install golang.org/x/tools/gopls@latest
+go install github.com/fatih/gomodifytags@latest
+go install github.com/x-motemen/gore/cmd/gore@latest
+go install github.com/cweill/gotests/gotests@latest
+npm install -g stylelint-cli
+npm -g install js-beautify
