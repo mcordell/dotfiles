fde77a27b601c515a1b74ab1fc1424b82c3848c8
Michael Cordell <mike@mikecordell.com>
2026-02-04 18:35:25 -0800

    nix: move last of zsh setup

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -1,6 +1,23 @@
 # Common Home Manager configuration shared across all systems
 { config, pkgs, ... }:
 
+let
+  # Fetch zsh-histdb plugin
+  zsh-histdb = pkgs.fetchFromGitHub {
+    owner = "larkery";
+    repo = "zsh-histdb";
+    rev = "90a6c104d0fcc0410d665e148fa7da28c49684eb";
+    sha256 = "sha256-vtG1poaRVbfb/wKPChk1WpPgDq+7udLqLfYfLqap4Vg=";
+  };
+
+  # Fetch forgit - interactive git commands with fzf
+  forgit = pkgs.fetchFromGitHub {
+    owner = "wfxr";
+    repo = "forgit";
+    rev = "53da496336305b8896e9cc2c8c5fbb016f31847c";  # v24.12.0
+    sha256 = "sha256-ERVSEmFfjkcmNML8SVjDnvZYxARaukiie2/lnKHJy58=";
+  };
+in
 {
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -15,13 +32,22 @@
     git
     neovim
     less
+    sqlite  # Required for zsh-histdb
   ];
+
+  # Place zsh plugins in .zsh/plugins directory
+  # Sourced directly in initContent since nix-managed prezto runs from nix store
+  home.file.".zsh/plugins/zsh-histdb".source = zsh-histdb;
+  home.file.".zsh/plugins/forgit".source = forgit;
+
+  # Powerlevel10k configuration
+  home.file.".p10k.zsh".source = ./../../zsh/.p10k.zsh;
+  home.file.".zsh/functions".source = ./../../zsh/functions;
+  home.file.".zsh/zsh_path.zsh".source = ./../../zsh/zsh_path.zsh;
 
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
 
     shellAliases = {
       # ruby/rails
@@ -29,13 +55,11 @@
       berspec = "nocorrect bundle exec rspec";
       bp = "bundle exec rails_best_practices --format html; open rails_best_practices_output.html";
       randpw = "openssl rand -base64";
-      reset_db = "./bin/rails db:drop RAILS_ENV=test; ./bin/rails db:create RAILS_ENV=test; ./bin/rails db:migrate RAILS_ENV=te
-st";
+      reset_db = "./bin/rails db:drop RAILS_ENV=test; ./bin/rails db:create RAILS_ENV=test; ./bin/rails db:migrate RAILS_ENV=test";
       rspec = "nocorrect rspec";
 
       # git
-      clean_branches = "git checkout \${DEFAULT_GIT_BRANCH:=\"master\"} && git recentb | fzf -m --ansi | awk '{ print $3 }' | x
-args git branch -D";
+      clean_branches = "git checkout \${DEFAULT_GIT_BRANCH:=\"master\"} && git recentb | fzf -m --ansi | awk '{ print $3 }' | xargs git branch -D";
       clean_orig = "find . -iregex .*\\.orig -exec rm -rf {} \\;";
       clear_gemlock = "git reset HEAD -- Gemfile.lock; git checkout -- Gemfile.lock; bundle";
       fu = "git log --oneline | fzf | awk '{ print $1 }' | xargs -I{} git commit --fixup={}";
@@ -124,9 +148,7 @@ args git branch -D";
       # PATH must run before instant prompt so brew/commands are available at first prompt
       source ~/.zsh/zsh_path.zsh
 
-
       files=(
-        "''${HOME}/.zsh/zprezto_init"
         "''${HOME}/.zsh_this_computer"
         "''${HOME}/.zsh/gnupg.zsh"
         "''${HOME}/.zsh/git_keys"
@@ -135,17 +157,98 @@ args git branch -D";
       for f ($^files(.N)) source $f
       unset files
 
+      # Forgit - interactive git commands with fzf (https://github.com/wfxr/forgit)
+      # Add completions to fpath before sourcing (for git forgit tab completion)
+      fpath=(~/.zsh/plugins/forgit/completions $fpath)
       source ~/.zsh/plugins/forgit/forgit.plugin.zsh
-
-      fpath=($HOME/.zsh/dot $fpath)
-      source $HOME/.zsh/dot/dot.sh
 
       fpath=( "$HOME/.zsh/functions" "''${fpath[@]}" )
       autoload -U $fpath[1]/*(.:t)
 
+      # zsh-histdb configuration (from https://github.com/larkery/zsh-histdb)
+      # Required on macOS - must be set before sourcing
+      HISTDB_TABULATE_CMD=(sed -e $'s/\x1f/\t/g')
+
+      # Source zsh-histdb from nix-managed location
+      source ~/.zsh/plugins/zsh-histdb/sqlite-history.zsh
+      autoload -Uz add-zsh-hook
+
+      # Custom autosuggest strategy using histdb
+      _zsh_autosuggest_strategy_histdb_top() {
+          local query="
+              select commands.argv from history
+              left join commands on history.command_id = commands.rowid
+              left join places on history.place_id = places.rowid
+              where commands.argv LIKE '$(sql_escape $1)%'
+              group by commands.argv, places.dir
+              order by places.dir != '$(sql_escape $PWD)', count(*) desc
+              limit 1
+          "
+          suggestion=$(_histdb_query "$query")
+      }
+      ZSH_AUTOSUGGEST_STRATEGY=histdb_top
+
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
       [[ ! -f ~/.oai ]] || source ~/.oai
     '';
+
+    # Prezto configuration - replaces .zpreztorc
+    prezto = {
+      enable = true;
+      
+      # Color output (auto set to 'no' on dumb terminals)
+      color = true;
+
+      # Prezto modules to load (order matters)
+      pmodules = [
+        "environment"
+        "terminal"
+        "editor"
+        "history"
+        "directory"
+        "spectrum"
+        "utility"
+        "completion"
+        "git"
+        "homebrew"
+        "history-substring-search"
+        "autosuggestions"
+        "osx"
+        "rsync"
+        "prompt"
+      ];
+
+      # Editor module configuration
+      editor = {
+        keymap = "vi";
+        promptContext = true;
+      };
+
+      # Git module configuration
+      git = {
+        submoduleIgnore = "all";
+      };
+
+      # Prompt module configuration
+      prompt = {
+        theme = "powerlevel10k";
+      };
+
+      # SSH module configuration
+      ssh = {
+        identities = [ "id_rsa" "id_rsa2" "id_github" ];
+      };
+
+      # Additional zstyle configurations not covered by home-manager options
+      extraConfig = ''
+        # Autosuggestions color configuration
+        zstyle ':prezto:module:autosuggestions' color 'yes'
+        zstyle ':prezto:module:autosuggestions:color' found 'fg=10'
+
+        # OS X module - Dash.app man pages keyword
+        zstyle ':prezto:module:osx:man' dash-keyword 'manpages'
+      '';
+    };
   };
 
   # HM-managed integrations (init added to .zshrc automatically)
