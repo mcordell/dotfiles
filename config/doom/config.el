d99aa9018771a8e01c6d9e90c938d99d5adc0b1d
Michael Cordell <mcordell@q-centrix.com>
2026-08-01 09:45:17 -0700

    doom: add meridian to agenda

--- config/doom/config.el
+++ config/doom/config.el
@@ -13,7 +13,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/"
-      org-agenda-files '("~/org/" "~/org/work/" "~/org/work/people/" "~/org/work/mro/" "~/org/work/mro/products/")
+      org-agenda-files '("~/org/" "~/org/work/" "~/org/work/people/" "~/org/work/mro/" "~/org/work/mro/products/" "~/org/work/projects/meridian.org")
       zot_bib "~/org/mylibrary/mylibrary.bib" 
       )
 
